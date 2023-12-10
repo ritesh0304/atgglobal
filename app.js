5b7c452a989c03a6f92f0815d0f1e15ab0d74172
ritesh0304 <90971150+ritesh0304@users.noreply.github.com>
2023-12-10 17:18:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Add your JavaScript in script.js
+
+function toggleMenu() {
+    var head2 = document.getElementById('head2');
+    var elm1=document.getElementById('notification');
+    var elm2=document.getElementById('search');
+
+    head2.classList.toggle('show');
+    elm1.classList.toggle('show');
+    elm2.classList.toggle('show');
+  }
+  
